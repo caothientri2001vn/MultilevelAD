b6576802e8e68dd2b5f17a98ae420958f5f7102d
huytrinhm <huytrinhm@gmail.com>
2025-05-16 01:10:53 +0700

    Fix inconsistent script

--- baselines/skip-ganomaly/merge_csv.py
+++ baselines/skip-ganomaly/merge_csv.py
@@ -5,7 +5,11 @@ import sys
 csv1 = pd.read_csv(sys.argv[1])
 csv2 = pd.read_csv(sys.argv[2])
 
-merged_csv = pd.merge(csv1, csv2, on='Path')
+merged_csv = pd.merge(csv1, csv2, on='Path', how='outer', suffixes=('_csv1', ''))
+
+for col in csv1.columns:
+    if col != 'Path' and col in csv2.columns:
+        merged_csv.drop(f"{col}_csv1", axis=1, inplace=True)
 
 merged_csv.to_csv(sys.argv[3], index=False)
 
